87f04a88e131922e6258cee114ff7756da2bf928
Josh Perry <joshperry@outlook.com>
2018-08-10 22:54:18 +0100

    Added a build script

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$source = "src"
+
+$shipping = "shipping"
+$cleanSource = "$shipping/src"
+$loveFile = "$shipping/game.love"
+
+Remove-Item $shipping -Recurse -ErrorAction Ignore
+
+New-Item $cleanSource -ItemType "Directory"
+
+Copy-Item -Recurse -Exclude @('*.moon', '*.tmx', '*.aseprite') "$source/*" $cleanSource
+
+Compress-Archive "$cleanSource/*" "$shipping/bin.zip"
+Rename-Item "$shipping/bin.zip" "game.love"
